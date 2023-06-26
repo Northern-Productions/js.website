8e3f80b3cf5b5189ae13ac467bac7661a27944bf
Rob Schultz <Robschultz4444@yahoo.com>
2023-06-26 17:16:16 -0500

    Added popup modals and CSS for them

--- js/main.js
+++ js/main.js
@@ -191,6 +191,6 @@ for (const elm of openModal) {
 
 for (const elm of closeModal) {
     elm.addEventListener('click', function() {
-        this.parentElement.parentElement.classList.remove(isVisible);
+        this.parentElement.parentElement.parentElement.classList.remove(isVisible);
     })
 }
